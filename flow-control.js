e8bffdef578603593987e57ff1499ab7962d25d2
Nicholas Caponi <iamnickygrey@gmail.com>
2020-11-12 19:22:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,23 +1,7 @@
-function basicTeenager(age) {
-if (age >= 13 && age <= 19) {
-return "You are a teenager!";
+if(age > 0 && age < 12){
+  console.log(`${name} is a child`);
+} else if(age >= 13 && age <= 19){
+  console.log(`${name} is a teenager`);
+} else {
+  console.log(`${name} is an adult`);
 }
-}
-function teenager(age) {
-if (age>=13 && age <= 19){
-return "You are a teenager!";
-} else {return "You are not a teenager";}
-}
-
-function ageChecker(age) {
-if (age>=13 && age <= 19){
-return "You are a teenager!";
-} else if (age <= 12){
-return "You are a kid";
-} else {return "You are a grownup";
-}
-
-function ternaryTeenager(age) {
-if (age >= 13 && age <= 19){
-return "You are a teenager";
-} else {return "You are not a teenager";}
