91e9c8813a1e2b451ede93b79228ce515e815c2a
Nicholas Caponi <iamnickygrey@gmail.com>
2020-11-12 19:39:45 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,10 +14,8 @@ if (age>=13 && age <= 19){
 return "You are a teenager!";
 } else if (age <= 12){
 return "You are a kid";
-} else {return "You are a grownup";
-}
+} else {return "You are a grownup";}
 
 function ternaryTeenager(age) {
 if (age >= 13 && age <= 19){
-return "You are a teenager";
-} else {return "You are not a teenager";}
+return "You are a teenager";} else {return "You are not a teenager";}
