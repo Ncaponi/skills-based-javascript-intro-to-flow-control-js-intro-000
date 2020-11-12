06ab4ff6ef624231c0ab544f094e2d07f13711c8
Nicholas Caponi <iamnickygrey@gmail.com>
2020-11-12 19:34:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,4 @@
-function basicTeenager(age){
-if basicTeenager (age >= 13 && age <=19) {
-return "You are a teenager!"
-}
-
-function teenager(age){
-if teenager(age >= 13 && age <= 19)}
-return "You are a teenager!" {
-else if {
-return "You are not a teenager"
+function basicTeenager(age) {
+if (age >= 13 && age <= 19) {
+return "You are a teenager!";
 }
