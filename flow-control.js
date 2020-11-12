b8634bd66fc78911a0d227194a3e000c58928c89
Nicholas Caponi <iamnickygrey@gmail.com>
2020-11-12 19:23:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,2 @@
-if(age > 0 && age < 12){
-  console.log(`${name} is a child`);
-} else if(age >= 13 && age <= 19){
+else if(age >= 13 && age <= 19){
   console.log(`${name} is a teenager`);
-} else {
-  console.log(`${name} is an adult`);
-}
